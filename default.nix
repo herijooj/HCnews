19486c077a50622ec2577fbf8ef8e1f6d804ff1e
Heric Camargo <heric_camargo@hotmail.com>
2024-01-11 23:55:30 -0300

    python packages nix-shell

--- default.nix
+++ default.nix
@@ -5,5 +5,9 @@ pkgs.mkShell {
     pkgs.pup
     pkgs.xmlstarlet
     pkgs.jq
+    pkgs.python3
+    pkgs.python311Packages.python-telegram-bot
+    pkgs.python311Packages.schedule
+    pkgs.python311Packages.httpx
   ];
 }
